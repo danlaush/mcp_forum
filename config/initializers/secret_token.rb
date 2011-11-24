576e31ac692ae6b1d7f53889537d7182301d517b
Dan Laush <dan.laush@gmail.com>
2011-11-24 12:45:56 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+McpForum::Application.config.secret_token = '6aa0b098af62c5c507f2bb1f5a17ada40d7d297e7d940b3a29ec843194f52457bacf98c813fcbf20258c6dd2fe03cf3d151e997e59c8a828bb2ae9e43f8f67d0'
